14f5f86be4ecff1a4ef64e07c06ad48a115e50bc
Felix <felix.waidler@web.de>
2021-08-31 18:01:42 +0200

    Initial Commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<div
+  class="bg-gray-800 rounded-lg max-w-sm w-full p-7 flex flex-col mt-6 items-center"
+>
+  <div class="bg-gray-700 w-60 rounded-lg" style="aspect-ratio: 1/1">
+    <img
+      class="rounded-lg"
+      src="https://pbs.twimg.com/profile_images/1121395911849062400/7exmJEg4.png"
+      alt="svelte"
+    />
+  </div>
+  <div class="mt-4 flex flex-col text-center">
+    <span class="text-3xl text-white font-bold">Svelte Kit Tailwind</span>
+    <span class="text-gray-500">A SvelteKit Template with Tailwind CSS</span>
+  </div>
+</div>
