1de01a6330b0ffe56aab6d0abfef9cec6820c0d9
Felix <felix.waidler@web.de>
2021-09-01 13:50:16 +0200

    centered box

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <div
-  class="bg-gray-800 rounded-lg max-w-sm w-full p-7 flex flex-col mt-6 items-center"
+  class="bg-gray-800 rounded-lg max-w-sm w-full p-7 flex flex-col mt-6 items-center ml-auto mr-auto"
 >
   <div class="bg-gray-700 w-60 rounded-lg" style="aspect-ratio: 1/1">
     <img
